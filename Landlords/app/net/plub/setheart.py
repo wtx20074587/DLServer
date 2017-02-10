5a4359c4aae2a793c8648acb663f28871ec06c4e
WTX <wtx20074587@gmail.com>
2017-02-10 16:32:00 +0800

    测试保持心跳
    测试保持心跳成功

--- Landlords/app/net/plub/setheart.py
+++ Landlords/app/net/plub/setheart.py
@@ -3,12 +3,19 @@ import sys,os
 from firefly.server.globalobject import netserviceHandle,GlobalObject
 from models.sysModel import showMsg
 from models.userModel import setHeart,checkLogin
-
+count = 0
 @netserviceHandle
 def heart_2(_conn,data):
 	'''心跳请求接口'''
 	isLogin = checkLogin(_conn.transport.sessionno)
+
+	print "wtx isLogin=",isLogin
+
 	if isLogin==False:
 		return showMsg(-1, '您还未登录')
+
+	else:
+		print "wtx 用户已经登录了！开始心跳一次",++count
+
 	setHeart(_conn.transport.sessionno)
 	return showMsg(1, '')
---
+++ Landlords/doc/6.连接斗地主游戏服务器/5heartBeat.py
@@ -0,0 +1,83 @@
+#coding:utf8
+
+import time,threading,random,json
+
+from socket import AF_INET,SOCK_STREAM,socket
+from thread import start_new
+import threading
+import struct,sys
+HOST='52.199.191.77'
+PORT=843
+BUFSIZE=1024
+ADDR=(HOST , PORT)
+# 指令类型
+LOGIN = 1 #登录
+TIMER = 2 #心跳
+HANDLE = 3 #抢地主，强退等
+DEAL = 4 #请求发牌
+
+#新建socket连接
+client = socket(AF_INET,SOCK_STREAM)
+client.connect(ADDR)
+
+TEST_USERS = [
+"[1,[3, 'aaaaa1', '123456']]",
+"[1,[4, 'aaaaa2', '123456']]",
+"[1,[5,'aaaaa3','123456']]",
+"[1,[6,'aaaaa4','123456']]",
+"[1,[7,'aaaaa5','123456']]",
+"[1,[8,'aaaaa6','123456']]"
+]
+
+def sendData(sendstr,commandId):
+    HEAD_0 = chr(127)
+    HEAD_1 = chr(12)
+    HEAD_2 = chr(24)
+    HEAD_3 = chr(22)
+    ProtoVersion = chr(65)
+    ServerVersion = 1
+    sendstr = sendstr
+    data = struct.pack('!sssss3I',HEAD_0,HEAD_1,HEAD_2,\
+                       HEAD_3,ProtoVersion,ServerVersion,\
+                       len(sendstr)+4,commandId)
+    senddata = data+sendstr #wtx：最终发送过去的数据是：固定格式的包头+数据本身
+    return senddata
+
+def resolveRecvdata(data):
+    head = struct.unpack('!sssss3I',data[:17])
+    lenght = head[6]
+    print lenght
+    data = data[17:17+lenght]
+    data = data.split("(end)")[0]
+    return data
+
+s1 = time.time()
+def send(data, num):
+    client.sendall(sendData(data,num))
+
+def start():
+    #1.随机选取一个用户，然后将该用户登录
+    random_number = random.randint(0, len(TEST_USERS) - 1)
+    loginMsg = TEST_USERS[random_number]
+    send(loginMsg, LOGIN)
+    #2.用户登录之后，保持心跳
+    timer = threading.Timer(1,sendHeartBeat)
+    timer.start()
+
+def sendHeartBeat():
+    heartBeatMsg = "[2]"  # 用于保持心跳的数据格式
+    send(heartBeatMsg, TIMER)
+    global timer
+    timer = threading.Timer(5,sendHeartBeat)
+    timer.start()
+
+start()
+#for i in range(10):
+#    start_new(start,())
+while True:
+    message = client.recv(1024)#接收服务器返回的消息
+    message = resolveRecvdata(message)#解析消息，此时返回的message对象是string类型
+    message = eval(message) #将message对象由string类型转化成dict类型
+    status = message['s'] #状态
+    return_msg = message['m'] #返回信息
+    print eval("u'%s'" %(return_msg))
